3f24999f166b519102ef9a9c597df1d0381360c3
lucas vanneste <luskay59@iutnc-503-06.iutnc.site.univ-lorraine.fr>
2024-05-23 15:23:23 +0200

    test ejs

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+// Définir le moteur de vue sur EJS
+app.set('view engine', 'ejs');
+
+// Route de base
 app.get('/', (req, res) => {
-res.send('Hello World!');
+  res.render('index', { message: 'Hello World!' });
 });
+
 app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
